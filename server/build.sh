8e7c47989476f2a8e3744e9a1871ad837368a2ac
Nelson <nelsonsomto10@gmail.com>
2025-06-19 20:33:22 +0100

    ready for demo

---
+++ server/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o errexit
+set -ex  # Print commands as they run and exit on failure
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Apply DB migrations (apply)
+python manage.py migrate --no-input --settings=settings.settings.prod
+
+# Collect static files
+python manage.py collectstatic --no-input --settings=settings.settings.prod
