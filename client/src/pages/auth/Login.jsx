9be9ee1223af4a3013b45ace4a0202a738ab4b8e
Nelson <nelsonsomto10@gmail.com>
2025-11-24 16:49:18 +0100

    added home and back navigation for auth pages

--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -220,6 +220,33 @@ function Login() {
                   </button>
                 </div>
               </form>
+
+              <div className="w-[100%]  p-3 mt-4 text-center flex justify-evenly gap-6">
+                <Link to="/" className="text-gray-400">
+                  Go Home
+                </Link>
+
+                <Link
+                  to={-1}
+                  className="flex justify-center text-gray-400"
+                  style={{ alignItems: "center" }}
+                >
+                  <svg
+                    width="18"
+                    height="18"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
+                      fill="#b5b5b5"
+                      fill-opacity="0.54"
+                    />
+                  </svg>
+                  Go Back
+                </Link>
+              </div>
             </div>
           </div>
         </div>
